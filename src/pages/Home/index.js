a1525de8959ba8440864c52b0dbe79bb164e2f97
melodyWxy <18210711176@163.com>
2019-08-24 00:41:14 +0800

    merge

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,26 @@
+/*
+ * @Date: 2019-08-23 23:50:33
+ * @LastEditors: melodyWxy
+ * @LastEditTime: 2019-08-24 00:07:23
+ */
 import React from "react";
 import Count from './../../Components/Count';
+import PropTypes from 'prop-types';
 
-function Home() {
-  return (
-    <Count />
-  );
+
+export default  class Home extends React.Component{
+   static propTypes={
+    name:PropTypes.string
+  }
+  render(){
+    return (
+      <div>
+        home 
+      </div>
+    )
+  }
 }
 
-export default Home;
+Home.defaultProps = {
+  name: 'Stranger'
+};

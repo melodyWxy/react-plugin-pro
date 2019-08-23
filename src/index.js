273360b3d7379ddb6b54edfecba19ed4fada84ea
melodyWxy <18210711176@163.com>
2019-08-24 01:24:42 +0800

    jiejuechongtu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2019-08-24 00:42:05
  * @LastEditors: melodyWxy
- * @LastEditTime: 2019-08-24 00:51:24
+ * @LastEditTime: 2019-08-24 01:24:21
  */
 
 import React from 'react';
@@ -9,19 +9,12 @@ import ReactDOM from 'react-dom';
 import Home from './pages/Home';
 // import { Provider } from './utils/Store'
 import store from './utils/Store'
-<<<<<<< HEAD
-=======
 import { loading } from './utils/utils'
->>>>>>> 6e28a2c21affc19e8c72e18097ac8e5efa16ee48
 import { Provider } from './utils/melody-redux'
 
 function App (){
     return (
-<<<<<<< HEAD
-        <Provider store={store}>
-=======
         <Provider store={store} utils={{loading}}>
->>>>>>> 6e28a2c21affc19e8c72e18097ac8e5efa16ee48
             <Home />
         </Provider>
     )
